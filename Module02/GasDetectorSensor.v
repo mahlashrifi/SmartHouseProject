32b3b2e8cd5a71528b20f330712bd7fdc2731d61
mahlashrifi <mahla7997@gmail.com>
2021-01-22 02:15:25 -0700

    Add Gas detector module

--- Module02/GasDetectorSensor.v
+++ Module02/GasDetectorSensor.v
@@ -27,8 +27,25 @@ module GasDetectorSensor (
 	output [2:0] dout       // output data
 );
 
-	/* write your code here */
-	
+		reg [11:0] sequence = 12'b000000000000;
+		
+		always @(posedge clk or posedge rst)
+		begin
+		
+			if(rst == 1'b1)	dout = 3'b000;
+			
+			else
+				begin
+				
+					sequence = {sequence[10:0], din};
+					
+					if(sequence[9:0] == 10'b1011101010)	dout[0] = 1;
+					if(sequence[11:0] == 12'b101010010011)	dout[1] = 1;
+					if(sequence[8:0] == 12'b100100100)	dout[2] = 1;
+					
+				end
+				
+		end
 	/* write your code here */
 
 endmodule
