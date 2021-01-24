f856ebe453495f4948f50cb172ea7eefd6c1cc20
mahlashrifi <mahla7997@gmail.com>
2021-01-24 16:32:11 -0700

    solve problem of GasDetector

--- Module02/GasDetectorSensor.v
+++ Module02/GasDetectorSensor.v
@@ -24,24 +24,34 @@ module GasDetectorSensor (
 	input        arst     , // async reset
 	input        clk      , // clock posedge
 	input        din      , // input data 
-	output [2:0] dout       // output data
+	output [2:0] dout  , 	// output data
+	output [11:0] seq
+
 );
 
 		reg [11:0] sequence = 12'b000000000000;
+		reg [2:0] out = 3'b000;
 		
-		always @(posedge clk or posedge rst)
+		assign dout = out;
+	   assign seq = sequence;
+		
+		always @(posedge clk or posedge arst)
 		begin
 		
-			if(rst == 1'b1)	dout = 3'b000;
+		   sequence = {sequence[10:0],din};
+			
+			if(arst == 1'b1)	out = 3'b000;
 			
 			else
 				begin
 				
-					sequence = {sequence[10:0], din};
 					
-					if(sequence[9:0] == 10'b1011101010)	dout[0] = 1;
-					if(sequence[11:0] == 12'b101010010011)	dout[1] = 1;
-					if(sequence[8:0] == 12'b100100100)	dout[2] = 1;
+					
+					if(sequence[9:0] == 10'b1011101010)	  out ={dout[1:0], 1'b1};
+					if(sequence[11:0] == 12'b101010010011)	out = {dout[2], 1'b1, dout[0]};
+					if(sequence[8:0] == 12'b100100100)	out = {1'b1, dout[1], dout[0]};
+					
+
 					
 				end
 				
