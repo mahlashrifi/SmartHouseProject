dc8d194bcbe5cd8ad70429f8604519cee0f30511
mahlashrifi <mahla7997@gmail.com>
2021-01-25 08:37:23 -0700

    add windowShadeDegree module

--- Module04/WindowShadeDegree.v
+++ Module04/WindowShadeDegree.v
@@ -20,14 +20,62 @@
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
 
+
+/*function [3:0] shadeLevel (input[3:0] userLight);
+	
+	begin
+	   assign shadeLevel = 4'b0000;
+		
+		always @( userLight )
+			help = active_lights;
+	
+	   integer out = help/100*16 ;
+	   if(out == 16 ) out = 15;
+		
+		
+		if(out > 7)
+			begin
+				shadeLevel[3] = 1'b1;
+				out = out - 8;
+			end
+			
+		if(out > 3)
+	   	begin
+				shadeLevel[2] = 1'b1;
+				out = out - 4;
+			end
+		
+	   if(out > 1)
+	   	begin
+				shadeLevel[1] = 1'b1;
+				out = out - 2;
+			end		
+		
+		
+		if(out == 1)
+	   	begin
+				shadeLevel[0] = 1'b1;
+				out = out - 1;
+			end
+		
+	end
+endfunction	*/
+
+
 module WindowShadeDegree (
 	input  [3:0] tcode  , // time code    [table2 time code   ]
 	input  [3:0] ulight , // user light   [light degree mode  ]
 	output [3:0] wshade   // shade level  [window shade level ]
 );
 
-	/* write your code here */
+
+  
+  assign  wshade = tcode == 4'b0001 ? 4'b1111 :
+						 tcode == 4'b0010 ? 4'b1011 :
+					    tcode == 4'b0100 ? ulight :
+					    tcode == 4'b1000 ? 4'b0000 :
+				       4'b0000;
 	
-	/* write your code here */
+		
 
 endmodule
