10f346632694065ade2c1b27222f9fafabd4931f
mahlashrifi <mahla7997@gmail.com>
2021-01-25 08:31:35 -0700

    add LampState module

--- Module04/LampState.v
+++ Module04/LampState.v
@@ -25,8 +25,26 @@ module LampState (
 	output [15:0] lights_state    // state of lights is on
 );
 
-	/* write your code here */
+   //assign lights_state = help ;
+		
 	
-	/* write your code here */
-
+	assign lights_state[0] =  active_lights > 4'b0000 ? 1 : 0; 
+	assign lights_state[1] =  active_lights > 4'b0001 ? 1 : 0; 
+	assign lights_state[2] =  active_lights > 4'b0010 ? 1 : 0; 
+	assign lights_state[3] =  active_lights > 4'b0011 ? 1 : 0; 
+	assign lights_state[4] =  active_lights > 4'b0100 ? 1 : 0; 
+	assign lights_state[5] =  active_lights > 4'b0101 ? 1 : 0; 
+	assign lights_state[6] =  active_lights > 4'b0110 ? 1 : 0; 
+	assign lights_state[7] =  active_lights > 4'b0111 ? 1 : 0; 
+	assign lights_state[8] =  active_lights > 4'b1000 ? 1 : 0; 
+	assign lights_state[9] =  active_lights > 4'b1001 ? 1 : 0; 
+	assign lights_state[10] =  active_lights > 4'b1010 ? 1 : 0; 
+	assign lights_state[11] =  active_lights > 4'b1011 ? 1 : 0; 
+	assign lights_state[12] =  active_lights > 4'b1100 ? 1 : 0; 
+	assign lights_state[13] =  active_lights > 4'b1101 ? 1 : 0; 
+	assign lights_state[14] =  active_lights > 4'b1110 ? 1 : 0; 
+	assign lights_state[15] =  active_lights == 4'b1111 ? 1 : 0;
+	
+		
+	
 endmodule
