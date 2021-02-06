8c1aa98627461ad38fc47742a88bf9d4d7c604a9
mahlashrifi <mahla7997@gmail.com>
2021-02-05 17:25:40 -0700

    add LightDance module

--- Module05/LightDance.v
+++ Module05/LightDance.v
@@ -29,8 +29,39 @@ module LightDance (
 	output [7:0] qdata   // output data
 );
 
-	/* write your code here */
+	reg [7:0] dinput;
 	
-	/* write your code here */
+	if(load)
+		begin
+			dinput[7] = pdata[7];
+			dinput[6] = pdata[7];
+			dinput[5] = pdata[7];
+			dinput[4] = pdata[7];
+			dinput[3] = pdata[7];
+			dinput[2] = pdata[7];
+			dinput[1] = pdata[7];
+			dinput[0] = pdata[7];
+		end
+    
+	 else
+		begin
+			dinput[7] = qdata[0] ^ din;
+			dinput[6] = qdata[7] ;
+			dinput[5] = qdata[0] ^ qdata[6];
+			dinput[4] = qdata[0] ^ qdata[5];
+			dinput[3] = qdata[4];
+			dinput[2] = qdata[3];
+			dinput[1] = qdata[0] ^ qdata[2];
+			dinput[0] = qdata[0] ^ qdata[1];
+		end
 
+		DFlop d0(arst, clk, dinput[0], 1, dout[0]);
+		DFlop d1(arst, clk, dinput[1], 1, dout[1]);
+		DFlop d2(arst, clk, dinput[2], 1, dout[2]);
+		DFlop d3(arst, clk, dinput[3], 1, dout[3]);
+		DFlop d4(arst, clk, dinput[4], 1, dout[4]);
+		DFlop d5(arst, clk, dinput[5], 1, dout[5]);
+		DFlop d6(arst, clk, dinput[6], 1, dout[6]);
+		DFlop d7(arst, clk, dinput[7], 1, dout[7]);
+		
 endmodule
